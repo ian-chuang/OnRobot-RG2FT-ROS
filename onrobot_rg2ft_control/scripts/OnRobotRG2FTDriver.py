f90469dfe25373bffda0bd9a479c7b8e79775cc0
Ian Chuang <iantc104@gmail.com>
2023-06-01 23:19:31 +0000

    publish wrench from both fingers

--- onrobot_rg2ft_control/scripts/OnRobotRG2FTDriver.py
+++ onrobot_rg2ft_control/scripts/OnRobotRG2FTDriver.py
@@ -5,6 +5,7 @@ from onrobot_rg2ft_control.OnRobotRG2FT import OnRobotRG2FT
 from std_srvs.srv import Trigger, TriggerResponse, SetBool, SetBoolResponse
 from onrobot_rg2ft_msgs.srv import SetProximityOffsets, SetProximityOffsetsResponse
 from onrobot_rg2ft_msgs.msg import RG2FTCommand, RG2FTState
+from geometry_msgs.msg import Wrench
 
 class OnRobotRG2FTDriver:
 
@@ -12,6 +13,8 @@ class OnRobotRG2FTDriver:
         self.gripper = OnRobotRG2FT(ip, port)
 
         self.state_pub = rospy.Publisher('state', RG2FTState, queue_size=1)
+        self.left_wrench_pub = rospy.Publisher('left_wrench', Wrench, queue_size=1)
+        self.right_wrench_pub = rospy.Publisher('right_wrench', Wrench, queue_size=1)
 
         self.cmd_sub = rospy.Subscriber('command', RG2FTCommand, self.gripper.writeCommand)
 
@@ -26,6 +29,8 @@ class OnRobotRG2FTDriver:
             # Getting and publish the Gripper status
             state = self.gripper.readState()
             self.state_pub.publish(state)
+            self.publish_wrenches(state)
+
             rate.sleep()
 
     def restart_cb(self, req):
@@ -43,6 +48,26 @@ class OnRobotRG2FTDriver:
         rospy.loginfo("Setting proximity offsets.")
         self.gripper.setProximityOffsets(req.ProximityOffsetL, req.ProximityOffsetR)
         return SetProximityOffsetsResponse(success=None, message=None)  # TODO: implement
+    
+    def publish_wrenches(self, state):
+        left_wrench = Wrench()
+        left_wrench.force.x = state.FxL / 10
+        left_wrench.force.y = state.FyL / 10
+        left_wrench.force.z = state.FzL / 10
+        left_wrench.torque.x = state.TxL / 100
+        left_wrench.torque.y = state.TyL / 100
+        left_wrench.torque.z = state.TzL / 100
+
+        right_wrench = Wrench()
+        right_wrench.force.x = state.FxR / 10
+        right_wrench.force.y = state.FyR / 10
+        right_wrench.force.z = state.FzR / 10
+        right_wrench.torque.x = state.TxR / 100
+        right_wrench.torque.y = state.TyR / 100
+        right_wrench.torque.z = state.TzR / 100
+
+        self.left_wrench_pub.publish(left_wrench)
+        self.right_wrench_pub.publish(right_wrench)
 
 if __name__ == '__main__':
     rospy.init_node('onrobot_rg2ft_driver', anonymous=True)
